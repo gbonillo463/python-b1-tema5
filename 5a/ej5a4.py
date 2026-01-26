fe6fa2815aba4c0345820334f025964867459c13
gbonillo463 <spamgerard4@gmail.com>
2026-01-26 17:10:47 +0100

    ej 5a4 COMPLETADO

--- 5a/ej5a4.py
+++ 5a/ej5a4.py
@@ -46,64 +46,30 @@ Ejemplo:
         False False False
         True True True
 
-Enunciat:
-Crea una classe anomenada "DatabaseConnector" que representi un connector de
-base de dades genèric amb baix acoblament, fàcil de mantenir i estendre,
-Per tant, que permeti connectar-se a diferents tipus de bases de dades. 
-La classe ha de tenir un mètode "connect" que permeti establir
-una connexió amb una base de dades específica.
-
-A més, s'han de crear tres classes per a diferents tipus de bases de dades:
-"SQLDatabase", "PostgresDatabase", i "RedshiftDatabase". Cada classe de base de dades
-  ha de tenir una propietat "connected" que s'estableixi en True
-quan es connecta correctament a la base de dades.
-
-Mètodes i propietats:
-
-Classe "DatabaseConnector":
-    connect(database): connecta a una base de dades específica. El paràmetre
-    "database" ha de ser una instància d'una de les classes de base de dades
-    ("SQLDatabase", "PostgresDatabase" o "RedshiftDatabase").
-
-Classe "SQLDatabase":
-    connected: propietat booleana que indica si la connexió a la base de dades
-     es va establir correctament.
-
-Classe "PostgresDatabase":
-    connected: propietat booleana que indica si la connexió a la base de dades
-     es va establir correctament.
-
-Classe "RedshiftDatabase":
-    connected: propietat booleana que indica si la connexió a la base de dades
-     es va establir correctament.
-
-Exemple:
-    Entrada:
-        db_connector = DatabaseConnector()
-        sql_db = SQLDatabase()
-        postgres_db = PostgresDatabase()
-        redshift_db = RedshiftDatabase()
-        print(sql_db.connected, postgres_db.connected, redshift_db.connected)
-        db_connector.connect(sql_db)
-        db_connector.connect(postgres_db)
-        db_connector.connect(redshift_db)
-        print(sql_db.connected, postgres_db.connected, redshift_db.connected)
-
-    Sortida:
-        False False False
-        True True True
 """
 
 # Write class DatabaseConnector here
+class DatabaseConnector:
+    def __init__(self):
+        self.database = None
+
+    def connect(self, database):
+        self.database = database
+        database.connected = True
 
 # Write class SQLDatabase here
-
+class SQLDatabase:
+    def __init__(self):
+        self.connected = False
 
 # Write class PostgresDatabase here
-
-
+class PostgresDatabase:
+    def __init__(self):
+        self.connected = False
 # Write class RedshiftDatabase here
-
+class RedshiftDatabase:
+    def __init__(self):
+        self.connected = False
 
 # Por la propia naturaleza del ejercicios no se puede probar imprimiendo por pantalla, 
 # revisa los tests para revisar la corrección de tu propuesta de solución
